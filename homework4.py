8b99676c50e7cb6b366484df23b947f68d3b0265
VershininaAlina <Kirillastrov@gmail.com>
2021-02-26 14:48:43 +0700

    Update homework4.py

--- homework4.py
+++ homework4.py
@@ -5,18 +5,17 @@ from selenium.webdriver.common.by import By
 
 link = "http://localhost/litecart/admin/"
 
-try:
-    browser = webdriver.Chrome()
-    driver.implicitly_wait(5)
-    browser.get(link)
-    def is_element_present(driver, By.CssSelector(#box-apps-menu):
-      try:
+browser = webdriver.Chrome()
+browser.get(link)
+    
+def is_element_present(driver, By.CssSelector(#box-apps-menu)):
+  try:
     driver.find_element(By.CssSelector(#content > h1 > font))
     return True
   except NoSuchElementException:
     return False
 
-finally:
+  finally:
     time.sleep(10)
     browser.quit()
 
