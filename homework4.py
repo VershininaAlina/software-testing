e96dcea2bd1052b7c49635a6632ebef47bc4873c
VershininaAlina <Kirillastrov@gmail.com>
2021-02-26 22:33:40 +0700

    Update homework4.py

--- homework4.py
+++ homework4.py
@@ -7,17 +7,22 @@ link = "http://localhost/litecart/admin/"
 
 browser = webdriver.Chrome()
 browser.get(link)
-    
-def is_element_present(driver, By.CssSelector(#box-apps-menu)):
-  try:
-    driver.find_element(By.CssSelector(#content > h1 > font))
-    return True
-  except NoSuchElementException:
-    return False
+input1 = browser.find_element_by_xpath('//*[@id="box-login"]/form/div[1]/table/tbody/tr[1]/td[2]/span/input')
+input1.send_keys("admin")
+input2 = browser.find_element_by_xpath('//*[@id="box-login"]/form/div[1]/table/tbody/tr[2]/td[2]/span/input')
+input2.send_keys("admin")
+button = browser.find_element_by_xpath('//*[@id="box-login"]/form/div[2]/button')
+button.click()
 
-  finally:
-    time.sleep(10)
-    browser.quit()
+try:
+  items = browser.find_elements_by_id("app-")
+  for item in items:
+   item.click()
+
+finally:
+  time.sleep(20)
+  browser.quit()
+
 
 # не забываем оставить пустую строку в конце файла
  
